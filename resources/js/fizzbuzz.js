ac18b9bccb29c7d5517c225fabfee0d5090483c0
Rokorios <charles.cox@rokorium.com>
2023-06-10 00:44:19 -0500

    0.2
    Added JS functionality.
    Game now works
    Project Complete

--- resources/js/fizzbuzz.js
+++ resources/js/fizzbuzz.js
@@ -1,8 +1,14 @@
+let currentNumber = 0;
+let lastNumbers = [0,0,0,0,0,0,0,0,0,0];
+let score = 0;
+let maxNumber = 0;
+
 function askName() {
     //let person = prompt("Please enter your name", "Harry Potter");
     let person = 'Charles';
     if (person != null) {
     document.getElementById("fName").value = person;
+    document.getElementById("fMaxNum").value = 50;
     }
 }
 function swapBoxes() {
@@ -26,12 +32,93 @@ function swapBoxes() {
         document.getElementById("numbersBox").style.display='none';
     }
 }
-function submit() {
+function startGame() {
     //Grab max number
-    let maxNumber = document.getElementById("fMaxNum").value;
+    maxNumber = document.getElementById("fMaxNum").value;
     //Grab Username
     let player = document.getElementById("fName").value;
     //Debug Message
     console.log(`Game Start! \nMax Number is ${maxNumber}\nPlayer is ${player}`)
     swapBoxes();
+    //Set up gameboard
+    document.getElementById("currentNumbers").innerHTML=`Current Number: ${currentNumber}`;
+    document.getElementById("lastNumbers").innerHTML = `Last Numbers: ${lastNumbers[0]}..${lastNumbers[1]}..${lastNumbers[2]}..${lastNumbers[3]}..${lastNumbers[4]}..${lastNumbers[5]}..${lastNumbers[6]}..${lastNumbers[7]}..${lastNumbers[8]}..${lastNumbers[9]}..`;
+
+}
+function determineFizzBuzz() {
+    if(currentNumber % 3 == 0 && currentNumber % 5 == 0){
+        return('FIZZBUZZ');
+    }
+    else if(currentNumber % 3 == 0){
+        return('FIZZ');
+    }
+    else if(currentNumber % 5 == 0) {
+        return('BUZZ')
+    }
+    else {
+        return(currentNumber);
+    }
+}
+//Function for FIZZ
+function FIZZ() {
+    if(determineFizzBuzz() == 'FIZZ'){
+        console.log("CORRECT");
+        score++;
+    }
+    updateNumbers();
+}
+//Function for FIZZ BUZZ
+function FIZZBUZZ() {
+    if(determineFizzBuzz() == 'FIZZBUZZ'){
+        console.log("CORRECT");
+        score++;
+    }
+    updateNumbers();
+}
+//Function for BUZZ
+function BUZZ() {
+    if(determineFizzBuzz() == 'BUZZ'){
+        console.log("CORRECT");
+        score++;
+    }
+    updateNumbers();
+}
+//Function for Reg Num
+function regNum() {
+    if(determineFizzBuzz() == currentNumber){
+        console.log("CORRECT");
+        score++;
+    }
+    updateNumbers();
+}
+//Function for Reload
+function resultMsg()   {
+    if(determineFizzBuzz() == 'BUZZ') {
+        return(`${currentNumber} is divisable by 5!`)
+    } else if (determineFizzBuzz() == 'FIZZ'){
+        return(`${currentNumber} is divisable by 3!`)
+    } else if (determineFizzBuzz() == 'FIZZBUZZ'){
+        return(`${currentNumber} is divisable by 3 and 5!`)
+    } else {
+        return(`${currentNumber} is not divisable by 3 or 5!`)
+    }
+}
+//Funciton for updating last numbers
+function updateNumbers() {
+    document.getElementById("result").innerHTML = determineFizzBuzz();
+    document.getElementById("resultExplain").innerHTML = resultMsg();
+
+    lastNumbers.unshift(determineFizzBuzz())
+    if(currentNumber < maxNumber){
+    currentNumber++;
+    }
+    else {alert('GAME OVER!')
+        document.getElementById("btnFIZZ").setAttribute("disabled",true);
+        document.getElementById("btnBUZZ").setAttribute("disabled",true);
+        document.getElementById("btnFIZZBUZZ").setAttribute("disabled",true);
+        document.getElementById("btnRegNum").setAttribute("disabled",true);        
+    }
+    document.getElementById("currentNumbers").innerHTML=`Current Number: ${currentNumber}`;
+    document.getElementById("lastNumbers").innerHTML = `Last Numbers: ${lastNumbers[0]}..${lastNumbers[1]}..${lastNumbers[2]}..${lastNumbers[3]}..${lastNumbers[4]}..${lastNumbers[5]}..${lastNumbers[6]}..${lastNumbers[7]}..${lastNumbers[8]}..${lastNumbers[9]}..`;
+    document.getElementById("score").innerHTML = score;
 }
